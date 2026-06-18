ed248795808d22403c4f058f3e2db065888a3f46
Gabryel Veríssimo <gabryelverissimo12@gmail.com>
2026-06-18 22:12:47 +0100

    style: organise imports in alembic env.py

--- migrations/env.py
+++ migrations/env.py
@@ -1,9 +1,7 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
 from alembic import context
+from sqlalchemy import engine_from_config, pool
 
 from app import models  # noqa: F401  (import side effect: register all mappers)
 from app.config import settings
